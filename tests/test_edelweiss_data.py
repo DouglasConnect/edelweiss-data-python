43289a88c100aa34ff2b2eba322d935976c3856a
Daniel Bachler <daniel@danielbachler.de>
2020-02-13 12:20:08 +0100

    Add first working test to upload a dataset from a csv file and verify the schema and data

--- tests/test_edelweiss_data.py
+++ tests/test_edelweiss_data.py
@@ -4,6 +4,9 @@ import pytest
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
+import filecmp
+import json
+import os
 
 # Helper for retrying requests
 def requests_retry_session(
@@ -17,7 +20,7 @@ def requests_retry_session(
         total=retries,
         read=retries,
         connect=retries,
-        backoff_factor=backoff_factor,
+        backoff_factor=backoff_factor, # backoff_factor is used as (backoff_factor * 2**retry_number) seconds
         status_forcelist=status_forcelist,
     )
     adapter = HTTPAdapter(max_retries=retry)
@@ -27,9 +30,9 @@ def requests_retry_session(
 
 @pytest.fixture
 def api():
-    edelweiss_api_url = 'http://localhost:8000'
+    edelweiss_api_url = 'http://192.168.1.247:8000'
     # The edelweiss server or the database may not be up yet so
-    # we manually request the /ready endpoint up to 8 times with backoff before
+    # we manually request the /ready endpoint up to 5 times with backoff before
     # we try to initialize the API
     response = requests_retry_session(retries=5).get('{}/ready'.format(edelweiss_api_url))
     response.raise_for_status()
@@ -43,3 +46,20 @@ def test_version():
 
 def test_connection(api):
     assert True
+
+def create_dataset_and_check_data(api, metadata, datasetname, input_filename, output_filename, output_schema_filename, expected_filename, expected_schema_filename):
+    with open (input_filename) as f:
+        dataset: PublishedDataset = api.create_published_dataset_from_csv_file(datasetname, f, metadata)
+    os.makedirs(os.path.dirname(output_filename), exist_ok=True)
+    with open (output_filename, 'w') as f:
+        data = dataset.get_raw_data()
+        json.dump(data, f, indent=2, sort_keys=True)
+    assert filecmp.cmp(expected_filename, output_filename)
+    with open (output_schema_filename, 'w') as f:
+        data = dataset.schema.encode()
+        json.dump(data, f, indent=2, sort_keys=True)
+    assert filecmp.cmp(expected_schema_filename, output_schema_filename)
+
+def test_roundtrip(api):
+    metadata = {"category": "alpha", "number": 42.0}
+    create_dataset_and_check_data(api, metadata, "small1", "tests/files/small1.csv", "tests/ouput/small1.actual-data.json", "tests/ouput/small1.actual-schema.json", "tests/files/small1.expected-data.json", "tests/files/small1.expected-schema.json")
